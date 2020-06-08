14384aa5cbacbe067d48bce2379889d8f5b70d59
Epafraditus Memoriano <epamemo@gmail.com>
2020-06-08 23:46:38 +0700

    Integrasi header footer dan index

--- Web-App/dashboard/profil.php
+++ Web-App/dashboard/profil.php
@@ -1,15 +1,4 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<!--
-Design by TEMPLATED
-http://templated.co
-Released for free under the Creative Commons Attribution License
-
-Name       : Hyacinth 
-Description: A two-column, fixed-width design with dark color scheme.
-Version    : 1.0
-Released   : 20131010
-
--->
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
@@ -112,10 +101,7 @@ Released   : 20131010
             <div class="title">
                 <h2>Welcome to our website</h2>
             </div>
-            <p>This is <strong>Hyacinth</strong>, a free, fully standards-compliant CSS template designed by <a
-                    href="http://templated.co" rel="nofollow">TEMPLATED</a>. The photos in this template are from <a
-                    href="http://fotogrph.com/"> Fotogrph</a>. This free template is released under the <a
-                    href="http://templated.co/license">Creative Commons Attribution</a> license, so you're pretty much
+            <p>This is <strong>Hyacinth</strong>, a free, fully standards-compliant CSS template designed by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>. The photos in this template are from <a href="http://fotogrph.com/"> Fotogrph</a>. This free template is released under the <a href="http://templated.co/license">Creative Commons Attribution</a> license, so you're pretty much
                 free to do whatever you want with it (even use it commercially) provided you give us credit for it. Have
                 fun :) </p>
         </div>
@@ -164,8 +150,7 @@ Released   : 20131010
         </div>
     </div>
     <div id="copyright" class="container">
-        <p>&copy; Untitled. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by <a
-                href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
+        <p>&copy; Untitled. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
     </div>
 </body>
 
---
+++ Web-App/index.php
@@ -0,0 +1,125 @@
+<?php
+include 'header.php';
+?>
+
+<div id="three-column" class="container">
+    <div><span class="arrow-down"></span></div>
+    <div id="tbox1"> <span class="icon icon-wrench"></span>
+        <div class="title">
+            <h2>Maecenas luctus</h2>
+        </div>
+        <p>Nullam non wisi a sem semper eleifend. Donec mattis libero eget urna. Duis pretium velit ac suscipit
+            mauris. Proin eu wisi suscipit nulla suscipit interdum.</p>
+        <a href="#" class="button">Learn More</a>
+    </div>
+    <div id="tbox2"> <span class="icon icon-cogs"></span>
+        <div class="title">
+            <h2>Integer gravida</h2>
+        </div>
+        <p>Proin eu wisi suscipit nulla suscipit interdum. Nullam non wisi a sem semper suscipit eleifend. Donec
+            mattis libero eget urna. Duis pretium velit ac mauris.</p>
+        <a href="#" class="button">Learn More</a>
+    </div>
+    <div id="tbox3"> <span class="icon icon-legal"></span>
+        <div class="title">
+            <h2>Praesent mauris</h2>
+        </div>
+        <p>Donec mattis libero eget urna. Duis pretium velit ac mauris. Proin eu wisi suscipit nulla suscipit
+            interdum. Nullam non wisi a sem suscipit semper eleifend.</p>
+        <a href="#" class="button">Learn More</a>
+    </div>
+</div>
+</div>
+<div id="wrapper3">
+    <div id="portfolio" class="container">
+        <div class="title">
+            <h2>Design Portfolio</h2>
+            <span class="byline">Integer sit amet pede vel arcu aliquet pretium</span>
+        </div>
+        <div class="column1">
+            <div class="box"> <a href="#"><img src="images/scr01.jpg" alt="" class="image image-full" /></a>
+                <h3>Vestibulum venenatis</h3>
+                <p>Fermentum nibh augue praesent a lacus at urna congue rutrum.</p>
+                <a href="#" class="button button-small">Etiam posuere</a>
+            </div>
+        </div>
+        <div class="column2">
+            <div class="box"> <a href="#"><img src="images/scr01.jpg" alt="" class="image image-full" /></a>
+                <h3>Praesent scelerisque</h3>
+                <p>Vivamus fermentum nibh in augue praesent urna congue rutrum.</p>
+                <a href="#" class="button button-small">Etiam posuere</a>
+            </div>
+        </div>
+        <div class="column3">
+            <div class="box"> <a href="#"><img src="images/scr01.jpg" alt="" class="image image-full" /></a>
+                <h3>Donec dictum metus</h3>
+                <p>Vivamus fermentum nibh in augue praesent urna congue rutrum.</p>
+                <a href="#" class="button button-small">Etiam posuere</a>
+            </div>
+        </div>
+        <div class="column4">
+            <div class="box"> <a href="#"><img src="images/scr01.jpg" alt="" class="image image-full" /></a>
+                <h3>Mauris vulputate dolor</h3>
+                <p>Rutrum fermentum nibh in augue praesent urna congue rutrum.</p>
+                <a href="#" class="button button-small">Etiam posuere</a>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="wrapper2">
+    <div id="welcome" class="container">
+        <div class="title">
+            <h2>Welcome to our website</h2>
+        </div>
+        <p>This is <strong>Hyacinth</strong>, a free, fully standards-compliant CSS template designed by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>. The photos in this template are from <a href="http://fotogrph.com/"> Fotogrph</a>. This free template is released under the <a href="http://templated.co/license">Creative Commons Attribution</a> license, so you're pretty much
+            free to do whatever you want with it (even use it commercially) provided you give us credit for it. Have
+            fun :) </p>
+    </div>
+    <div id="newsletter" class="container">
+        <div class="title">
+            <h2>Newsletter Signup</h2>
+            <span class="byline">Integer sit amet pede vel arcu aliquet pretium</span>
+        </div>
+        <div class="content">
+            <form method="post" action="#">
+                <div class="row half">
+                    <div class="6u">
+                        <input type="text" class="text" name="name" placeholder="Name" />
+                    </div>
+                    <div class="6u">
+                        <input type="text" class="text" name="email" placeholder="Email" />
+                    </div>
+                </div>
+                <div class="row half">
+                    <div class="12u">
+                        <textarea name="message" placeholder="Message"></textarea>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="12u"> <a href="#" class="button submit">Send Message</a> </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<div id="wrapper4">
+    <div id="footer" class="container">
+        <div>
+            <header class="title">
+                <h2>Get in touch</h2>
+                <span class="byline">Phasellus nec erat sit amet nibh pellentesque congue</span>
+            </header>
+            <ul class="contact">
+                <li><a href="#" class="icon icon-twitter"><span>Twitter</span></a></li>
+                <li><a href="#" class="icon icon-facebook"><span></span></a></li>
+                <li><a href="#" class="icon icon-dribbble"><span>Pinterest</span></a></li>
+                <li><a href="#" class="icon icon-tumblr"><span>Google+</span></a></li>
+                <li><a href="#" class="icon icon-rss"><span>Pinterest</span></a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<?php
+include 'footer.php';
+?>
