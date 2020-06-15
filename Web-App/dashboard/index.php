b5941eec31bdd4d8aa5d3fe393d6bb2e653290cc
Epafraditus Memoriano <epamemo@gmail.com>
2020-06-15 23:01:06 +0700

    create index for selecting the material

---
+++ Web-App/dashboard/index.php
@@ -0,0 +1,18 @@
+<?php
+include 'header.php';
+?>
+<div class="jumbotron jumbotron-fluid">
+    <h1>Hai Sultan, Selamat datang! Ayo lanjutkan belajarnya!</h1>
+</div>
+<div class="row">
+    <div class="col-6">
+        <a href="level.php" class="btn btn-primary" style="font-size:5.3vw; white-space: normal; word-wrap: break-word; width: 100%; height:50vh; ">Belajar Bahasa Jawa</a>
+    </div>
+    <div class="col-6">
+        <a href="level.php" class="btn btn-success" style="font-size:5.3vw; white-space: normal; word-wrap: break-word; width: 100%; height:50vh; ">Belajar Bahasa Sunda</a>
+    </div>
+</div>
+
+<?php
+include 'footer.php';
+?>
