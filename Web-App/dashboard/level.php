b0f5b7c695f34fafcfd6b5f3490caf9ad87078ed
Epafraditus Memoriano <epamemo@gmail.com>
2020-06-15 22:43:55 +0700

    create level page

---
+++ Web-App/dashboard/level.php
@@ -0,0 +1,46 @@
+<?php
+include 'header.php';
+?>
+
+<div class="row">
+    <div class="col-3">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Level 1</h5>
+                <p class="card-text">Pengenalan mengenai alphabet dalam bahasa jawa.</p>
+                <a href="#" class="btn btn-primary">Mulai Kuis</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-3">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Level 2</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Mulai Kuis</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-3">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Level 3</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Mulai Kuis</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-3">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Level 4</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Mulai Kuis</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+include 'footer.php';
+?>
