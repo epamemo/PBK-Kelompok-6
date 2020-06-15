7d3fb9a4a858f80ed91a5e5856ef482bae52ed13
angga adiansya <anggamu55@gmail.com>
2020-06-15 21:20:32 +0700

    profil
    profil dashboard

--- Web-App/dashboard/profil.php
+++ Web-App/dashboard/profil.php
@@ -2,6 +2,181 @@
 include 'header.php';
 ?>
 
+<body>
+    <div id="header-wrapper">
+        <div id="header" class="container">
+            <div id="logo">
+                <h1><a href="#">SIJANDA</a></h1>
+                <span>Sinau <a href="http://templated.co" rel="nofollow">Jawa & Sunda</a></span>
+            </div>
+
+            <div class="title">
+                <h2>keterangan</h2>
+                <p> sijanda merupakan aplikasi belajar bahasa sunda dan jawa </p>
+            </div>
+        </div>
+        <div class="">
+            <section>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-sm-10 col-md-6 p-b-35">
+                            <div class="flex-col-c-s p-b-35">
+                                <h3 class="t1-b-1 cl-3 m-b-11">SIJANDA</h3>
+                            </div>
+
+                            <div>
+                                <p class="t1-s-2 cl-6 m-b-10">
+                                    SIJANDA merupakan erupakan aplikasi belajar bahasa jawa dan sunda
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-sm-10 col-md-6 p-b-35">
+                            <div class="flex-col-c-s p-b-35">
+                                <h3 class="t1-b-1 cl-3 m-b-11">Mission & Vision</h3>
+                            </div>
+
+                            <div>
+                                <p class="t1-s-2 cl-6 m-b-10">
+                                    <span class="t1-s-5 cl-3">At present:</span> Visi dan Misi kami adalah mencerdaskan
+                                    berbahasa daerah
+                                </p>
+                                <p class="t1-s-2 cl-6 m-b-10">
+                                    <span class="t1-s-5 cl-3">In the future:</span> Visi dan Misi kami adalah menjaga
+                                    kelestarian bahasa daerah
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="bg-0 p-t-92 p-b-52">
+                <div class="container">
+                    <!-- Title section -->
+                    <div class="flex-col-c-c p-b-50">
+                        <h3 class="t1-b-1 cl-3 txt-center m-b-11">
+                            Meet Our Team
+                        </h3>
+
+                        <div class="size-a-2 bg-3"></div>
+                    </div>
+
+                    <!--  -->
+                    <div class="row justify-content-center">
+                        <div class="col-sm-6 col-md-5 col-lg-3 p-b-40">
+                            <div>
+                                <div class="wrap-pic-w pos-relative">
+                                    <img src="image/team-01.jpg" alt="IMG">
+                                </div>
+
+                                <div class="flex-col-c-c p-t-28">
+                                    <a class="t1-m-1 text-uppercase cl-3 txt-center">
+                                        M Zakki Mahatir
+                                    </a>
+                                    <br>
+                                    <span class="t1-s-5 cl-6 txt-center">
+                                        4611417047
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-6 col-md-5 col-lg-3 p-b-40">
+                            <div>
+                                <div class="wrap-pic-w pos-relative">
+                                    <img src="image/team-02.jpg" alt="IMG">
+                                </div>
+
+                                <div class="flex-col-c-c p-t-28 p-b-28">
+                                    <a class="t1-m-1 text-uppercase cl-3 txt-center">
+                                        Ibnu Azam Alfansuri
+                                    </a>
+                                    <br>
+                                    <span class="t1-s-5 cl-6 txt-center">
+                                        4611417051
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-6 col-md-5 col-lg-3 p-b-40">
+                            <div>
+                                <div class="wrap-pic-w pos-relative">
+                                    <img src="image/team-03.jpg" alt="IMG">
+                                </div>
+
+                                <div class="flex-col-c-c p-t-28">
+                                    <a class="t1-m-1 text-uppercase cl-3 txt-center">
+                                        Sulthan Daffa Firzatullah
+                                    </a>
+                                    <br>
+                                    <span class="t1-s-5 cl-6 txt-center">
+                                        4611417055
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-6 col-md-5 col-lg-3 p-b-40">
+                            <div>
+                                <div class="wrap-pic-w pos-relative">
+                                    <img src="Web-App/image/team-04.jpg" alt="IMG">
+                                </div>
+
+                                <div class="flex-col-c-c p-t-28">
+                                    <a class="t1-m-1 text-uppercase cl-3 txt-center">
+                                        Nur Hilmi Insan Muhammad
+                                    </a>
+                                    <br>
+                                    <span class="t1-s-5 cl-6 txt-center">
+                                        4611417062
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-6 col-md-5 col-lg-3 p-b-40">
+                            <div>
+                                <div class="wrap-pic-w pos-relative">
+                                    <img src="Web-App/image/team-04.jpg" alt="IMG">
+                                </div>
+
+                                <div class="flex-col-c-c p-t-28">
+                                    <a class="t1-m-1 text-uppercase cl-3 txt-center">
+                                        Angga Adiansya
+                                    </a>
+                                    <br>
+                                    <span class="t1-s-5 cl-6 txt-center">
+                                        4611417063
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-6 col-md-5 col-lg-3 p-b-40">
+                            <div>
+                                <div class="wrap-pic-w pos-relative">
+                                    <img src="Web-App/image/team-04.jpg" alt="IMG">
+                                </div>
+
+                                <div class="flex-col-c-c p-t-28">
+                                    <a class="t1-m-1 text-uppercase cl-3 txt-center">
+                                        Epafraditus Memoriano
+                                    </a>
+                                    <br>
+                                    <span class="t1-s-5 cl-6 txt-center">
+                                        4611417070
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+        </div>
+</body>
+
+
 
 <?php
 include 'footer.php';
