474a4e9aff8edf6f657af1dbcea404a2acd92750
nurhilmi14 <50134471+nurhilmi14@users.noreply.github.com>
2020-06-19 14:44:27 +0700

    Perbaiki Header LandingPage

--- Web-App/header.php
+++ Web-App/header.php
@@ -1,15 +1,23 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<!--
-
-
--->
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title></title>
-    <meta name="keywords" content="" />
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>siJanda</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
+    <!-- Third party plugin CSS-->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet" />
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
     <link href="css/default.css" rel="stylesheet" type="text/css" media="all" />
     <link href="css/fonts.css" rel="stylesheet" type="text/css" media="all" />
@@ -18,20 +26,25 @@
 
 </head>
 
-<body>
-    <div id="header-wrapper">
-        <div id="header" class="container">
-            <div id="logo">
-                <img src="../4x/logo-w-completed@4x.png" alt="" width="750px">
+<body id="page-top">
+
+    <header class="masthead">
+        <div class="container h-100">
+            <div class="row align-items-center justify-content-center text-center">
+                <div id="logo">
+                    <img src="../4x/logo-w-completed@4x.png" alt="" width="750px">
+                </div>
+                <div id="menu">
+                    <ul>
+                        <li class="current_page_item"><a href="index.php" accesskey="1" title="">Beranda</a></li>
+                        <li><a href="tentang.php" accesskey="2" title="">Tentang</a></li>
+                        <li><a href="#" accesskey="3" title="">Kontak</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-8 align-self-baseline">
+                    <a class="btn btn-light btn-xl js-scroll-trigger mx-2 my-2" href="#about">Masuk</a>
+                    <a class="btn btn-primary2 btn-xl js-scroll-trigger mx-2 my-2" href="#about">Daftar</a>
+                </div>
             </div>
-            <div id="menu">
-                <ul>
-                    <li class="current_page_item"><a href="#" accesskey="1" title="">Beranda</a></li>
-                    <li><a href="tentang.php" accesskey="2" title="">Tentang</a></li>
-                    <li><a href="#" accesskey="3" title="">Kontak</a></li>
-                </ul>
-            </div>
-            <br>
-            <br>
         </div>
-    </div>
+    </header>
