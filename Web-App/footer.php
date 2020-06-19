36de17944dc7e81b4d394abf08fa10d6da4b267c
nurhilmi14 <50134471+nurhilmi14@users.noreply.github.com>
2020-06-19 14:56:18 +0700

    Perbaiki Footer LandingPage

--- Web-App/footer.php
+++ Web-App/footer.php
@@ -1,27 +1,41 @@
-<div id="wrapper4">
-    <div id="footer" class="container">
-        <div>
-            <header class="title">
-                <h2 class="mb-4">#TEMANSEJANDA</h2>
-                <br>
-                <span class="byline">MUDA, GRATIS, NYAMAN, ENAK, MANTAP, MAKYUS</span><br>
-                <span class="byline">BEEUUHHH YEEEKAANN</span>
-            </header>
-            <ul class="contact">
-                <li><a href="#" class="icon fa-facebook"><span></span></a></li>
-                <li><a href="#" class="icon fa-underline"><span></span></a></li>
-                <li><a href="#" class="icon fa-copyright"><span></span></a></li>
-                <li><a href="#" class="icon fa-kaggle"><span></span></a></li>
-                <li><a href="#" class="icon fa-bold"><span></span></a></li>
-                <li><a href="#" class="icon fa-opera"><span></span></a></li>
-                <li><a href="#" class="icon fa-yahoo"><span></span></a></li>
-            </ul>
+<section class="page-section bg-light" id="wrapper4">
+    <div class="container" id="footer">
+        <div class="row justify-content-center">
+            <div class="col-lg-8 text-center">
+                <header class="title">
+                    <h2 class="mt-0 text-primary">#TEMANSEJANDA</h2>
+                    <hr class="divider my-4" />
+                    <p class="text-muted mb-5">MUDA, GRATIS, NYAMAN, ENAK, MANTAP, MAKYUS <br> BEEUUHHH YEEEKAANN</p>
+                </header>
+            </div>
         </div>
+        <ul class="contact">
+            <li><a href="#" class="icon fa-facebook"><span></span></a></li>
+            <li><a href="#" class="icon fa-underline"><span></span></a></li>
+            <li><a href="#" class="icon fa-copyright"><span></span></a></li>
+            <li><a href="#" class="icon fa-kaggle"><span></span></a></li>
+            <li><a href="#" class="icon fa-bold"><span></span></a></li>
+            <li><a href="#" class="icon fa-opera"><span></span></a></li>
+            <li><a href="#" class="icon fa-yahoo"><span></span></a></li>
+        </ul>
     </div>
-</div>
-<div id="copyright" class="container">
-    <p>&copy; Pembelajaran Berbantuan Komputer | Website by <a href="#">Kelompok 6</a> | Design by <a href="#">TEMANSEJANDA</a>.</p>
-</div>
+</section>
+
+<footer class="bg-primary">
+    <div class="container">
+        <div class="small text-center text-white">&copy; Pembelajaran Berbantuan Komputer | Website by <a href="#" class="text-white">Kelompok 6</a> | Design by <a href="#" class="text-white">TEMANSEJANDA</a>.</div>
+    </div>
+</footer>
+
+<!-- Bootstrap core JS-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+<!-- Third party plugin JS-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
+<!-- Core theme JS-->
+<script src="js/scripts.js"></script>
+
 </body>
 
 </html>
