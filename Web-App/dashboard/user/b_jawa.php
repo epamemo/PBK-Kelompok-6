6a8133c5ef58f66a9490368b0241f5dfb6e5f8a9
Epafraditus Memoriano <epamemo@gmail.com>
2020-06-23 22:59:26 +0700

    Update button for card

--- Web-App/dashboard/user/b_jawa.php
+++ Web-App/dashboard/user/b_jawa.php
@@ -14,7 +14,7 @@ include 'header.php';
     <div class="row">
 
         <div class="col-xl mb-4">
-            <div class="card border-bottom-primary shadow">
+            <div class="card border-bottom-primary shadow" style="height: 230px;">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
@@ -23,23 +23,22 @@ include 'header.php';
                         </div>
                     </div>
                 </div>
-                <div class="card-footer">
+                <div class="card-footer bg-white border-0">
                     <a href="#" class="btn btn-primary">Mulai</a>
                 </div>
             </div>
         </div>
         <div class="col-xl mb-4">
-            <div class="card border-bottom-primary shadow">
+            <div class="card border-bottom-primary shadow" style="height: 230px;">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-primary card-title h4">Level 2</div>
                             <div class="card-text h6 mb-4">Pengenalan kosakata yang digunakan sehari-hari</div>
-                            <a href="#" class="btn btn-primary">Mulai</a>
                         </div>
                     </div>
                 </div>
-                <div class="card-footer">
+                <div class="card-footer bg-white border-0">
                     <a href="#" class="btn btn-primary">Mulai</a>
                 </div>
             </div>
@@ -51,27 +50,25 @@ include 'header.php';
                         <div class="col mr-2">
                             <div class="text-primary card-title h4">Level 3</div>
                             <div class="card-text h6 mb-4">Penyusunan kosakata dalam membuat kalimat pernyataan</div>
-                            <a href="#" class="btn btn-primary">Mulai</a>
                         </div>
                     </div>
                 </div>
-                <div class="card-footer">
+                <div class="card-footer bg-white border-0">
                     <a href="#" class="btn btn-primary">Mulai</a>
                 </div>
             </div>
         </div>
         <div class="col-xl mb-4">
-            <div class="card border-bottom-primary shadow">
+            <div class="card border-bottom-primary shadow" style="height: 230px;">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-primary card-title h4">Level 4</div>
                             <div class="card-text h6 mb-4">Penyusunan kosakata dalam membuat kalimat pertanyaan.</div>
-                            <a href="#" class="btn btn-primary">Mulai</a>
                         </div>
                     </div>
                 </div>
-                <div class="card-footer">
+                <div class="card-footer bg-white border-0">
                     <a href="#" class="btn btn-primary">Mulai</a>
                 </div>
             </div>
