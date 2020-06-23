2885ab5ff8b9ab686cbfcc4abafdf5e7d4098a2e
Daffa999 <63541886+Daffa999@users.noreply.github.com>
2020-06-23 20:17:48 +0700

    database
    cek dah bener blm

--- database.sql
+++ database.sql
@@ -1,74 +1,118 @@
-CREATe DATABASE PBK;
-CREATE TABLE "User"
-(
-	id INT(11)
-	UNSIGNED AUTO_INCREMENT;
-	Nama VARCHAR
-	(40) NOT NULL;
-	Level INT
-	(1) NOT NULL;
-	E_mail VARCHAR
-	(40) NOT NULL;
-	Pwd VARCHAR
-	(30) NOT NULL;
-	PRIMARY KEY
-	(id);
-)
-	CREATE TABLE Admin
-	(
-		id INT(11)
-		UNSIGNED AUTO_INCREMENT;
-	Nama VARCHAR
-		(40) NOT NULL;
-	E_mail VARCHAR
-		(40) NOT NULL;
-	Pwd VARCHAR
-		(30) NOT NULL;
-	PRIMARY KEY
-		(id);
+CREATE DATABASE PBK;
+
+USE PBK;
+
+CREATE TABLE 'user'(
+	'id_user' int
+(11) UNSIGNED AUTO_INCREMENT,
+    'nama' varchar
+(40) NOT NULL,
+    'level' int
+(1) NOT NULL,
+    'E_mail' varchar
+(49) NOT NULL,
+    'Pwd' varchar
+(30) NOT NULL,
+    PRIMARY KEY
+('id_user')
 )
 
-		CREATE TABLE Bahasa
-		(
-			id_Bahasa INT(11)
-			UNSIGNED AUTO_INCREMENT;
-	Bahasa VARCHAR
-			(40) NOT NULL;
-	PRIMARY KEY
-			(id_Pelajaran);
+CREATE TABLE 'bahasa'(
+    'id_bahasa' int
+(11) UNSIGNED AUTO_INCREMENT,
+    'bahasa' varchar
+(40),
+    PRIMARY KEY
+('id_bahasa')
 )
-			CREATE TABLE Materi
-			(
-				id_Materi INT(11)
-				UNSIGNED AUTO_INCREMENT;
-	Judul VARCHAR
-				(40) NOT NULL;
-	Tipe VARCHAR
-				(40) NOT NULL;  
-	PRIMARY KEY
-				(id_Materi);
+
+CREATE TABLE 'materi'(
+	'id_materi' int
+(11) UNSIGNED AUTO_INCREMENT,
+    'Judul' varchar
+(60) NOT NULL,
+    'Tipe' varchar
+(40) NOT NULL,
+    PRIMARY KEY
+('id_materi')
 )
-				CREATE TABLE Kuis
-				(
-					id_Materi INT(11) NOT NUll;
-					Soal TEXT
-					(255) NOT NULL;
+
+CREATE TABLE 'kuis'(
+	'id_kuis' int
+(11) UNSIGNED AUTO_INCREMENT,
+    'soal' text
+(255) NOT NULL,
+    PRIMARY KEY
+('id_kuis') 
 )
-					CREATE TABLE Video
-					(
-						id_video INT(11)
-						UNSIGNED AUTO_INCREMENT;
-	judul_video VARCHAR
-						(255) NOT NULL;
-	url VARCHAR
-						(255) NOT NULL;
-	PRIMARY KEY
-						(id_video);
+
+CREATE TABLE 'video'(
+    'id_video' int
+(11) UNSIGNED AUTO_INCREMENT,  
+    'judul_video' varchar
+(255) NOT NuLL,
+    'url' varchar
+(255) NOT NULL,
+    PRIMARY KEY
+('id Video')
 )
-						CREATE TABLE History
-						(
-							id_user INT(11) NOT NULL;
-							id_materi INT
-							(11) NOT NULL;
+
+CREATE DATABASE PBK;
+
+USE PBK;
+
+CREATE TABLE 'user'(
+	'id_user' int
+(11) UNSIGNED AUTO_INCREMENT,
+    'nama' varchar
+(40) NOT NULL,
+    'level' int
+(1) NOT NULL,
+    'E_mail' varchar
+(49) NOT NULL,
+    'Pwd' varchar
+(30) NOT NULL,
+    PRIMARY KEY
+('id_user')
+)
+
+CREATE TABLE 'bahasa'(
+    'id_bahasa' int
+(11) UNSIGNED AUTO_INCREMENT,
+    'bahasa' varchar
+(40),
+    PRIMARY KEY
+('id_bahasa')
+)
+
+CREATE TABLE 'materi'(
+	'id_materi' int
+(11) UNSIGNED AUTO_INCREMENT,
+    'Judul' varchar
+(60) NOT NULL,
+    'Tipe' varchar
+(40) NOT NULL,
+    PRIMARY KEY
+('id_materi')
+)
+
+CREATE TABLE 'kuis'(
+	'id_kuis' int
+(11) UNSIGNED AUTO_INCREMENT,
+    'soal' text
+(255) NOT NULL,
+    PRIMARY KEY
+('id_kuis') 
+)
+
+CREATE TABLE 'video'(
+    'id_video' int
+(11) UNSIGNED AUTO_INCREMENT,  
+    'judul_video' varchar
+(255) NOT NuLL,
+    'url' varchar
+(255) NOT NULL,
+    PRIMARY KEY
+('id Video')
 )
 
