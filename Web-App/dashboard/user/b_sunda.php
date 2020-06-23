44aa8fa4c50b906d976d3de0b8398e5cc88bd8f3
nurhilmi14 <50134471+nurhilmi14@users.noreply.github.com>
2020-06-23 22:51:34 +0700

    Update lagi boooooorrrrrrrrrrrrrrrrrrrr

--- Web-App/dashboard/user/b_sunda.php
+++ Web-App/dashboard/user/b_sunda.php
@@ -15,54 +15,62 @@ include 'header.php';
     <div class="row contrainer">
 
         <div class="col-xl mb-4">
-            <div class="card border-bottom-primary shadow">
+            <div class="card border-bottom-primary shadow" style="height: 230px;">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-primary card-title h4">Level 1</div>
                             <div class="card-text h6 mb-4">Pengenalan mengenai alphabet dalam Bahasa Sunda</div>
-                            <a href="#" class="btn btn-primary">Mulai</a>
                         </div>
                     </div>
+                </div>
+                <div class="card-footer bg-white border-0">
+                    <a href="#" class="btn btn-primary">Mulai</a>
                 </div>
             </div>
         </div>
         <div class="col-xl mb-4">
-            <div class="card border-bottom-primary shadow">
+            <div class="card border-bottom-primary shadow" style="height: 230px;">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-primary card-title h4">Level 2</div>
                             <div class="card-text h6 mb-4">Pengenalan kosakata yang digunakan sehari-hari</div>
-                            <a href="#" class="btn btn-primary">Mulai</a>
                         </div>
                     </div>
+                </div>
+                <div class="card-footer bg-white border-0">
+                    <a href="#" class="btn btn-primary">Mulai</a>
                 </div>
             </div>
         </div>
         <div class="col-xl mb-4">
-            <div class="card border-bottom-primary shadow">
+            <div class="card border-bottom-primary shadow" style="height: 230px;">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-primary card-title h4">Level 3</div>
                             <div class="card-text h6 mb-4">Penyusunan kosakata dalam membuat kalimat pernyataan</div>
-                            <a href="#" class="btn btn-primary">Mulai</a>
                         </div>
                     </div>
+                </div>
+                <div class="card-footer bg-white border-0">
+                    <a href="#" class="btn btn-primary">Mulai</a>
                 </div>
             </div>
         </div>
         <div class="col-xl mb-4">
-            <div class="card border-bottom-primary shadow">
+            <div class="card border-bottom-primary shadow" style="height: 230px;">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-primary card-title h4">Level 4</div>
                             <div class="card-text h6 mb-4">Penyusunan kosakata dalam membuat kalimat pertanyaan.</div>
-                            <a href="#" class="btn btn-primary">Mulai</a>
                         </div>
                     </div>
+                </div>
+                <div class="card-footer bg-white border-0">
+                    <a href="#" class="btn btn-primary">Mulai</a>
                 </div>
             </div>
         </div>
