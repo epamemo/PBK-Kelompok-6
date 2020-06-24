60fd62c4720eabc4af39e50f691b629f2cb1da42
Epafraditus Memoriano <epamemo@gmail.com>
2020-06-24 14:31:46 +0700

    add material page for sundanese

---
+++ Web-App/dashboard/user/materi3_s.php
@@ -0,0 +1,26 @@
+<?php
+include 'header.php';
+?>
+
+<!-- Begin Page Content -->
+<div class="container">
+
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Materi 3 - Bahasa Jawa</h1>
+        <a href="index.php" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                class="fas fa-arrow-left fa-sm text-white-50"></i> Kembali</a>
+    </div>
+
+    <!-- Content -->
+    <div class="container-fluid">
+        <iframe width="420" height="315" src="https://www.youtube.com/embed/K2at9eV1qGU"></iframe>
+        <h1>Kosa Kata Lanjutan</h1>
+        <p>Penyusunan kosakata dalam membuat kalimat pernyataan </p>
+    </div>
+
+</div>
+
+<?php
+include 'footer.php';
+?>
