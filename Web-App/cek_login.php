35ed719e5190cb89c4a4a571588c4c49295848c3
Epafraditus Memoriano <epamemo@gmail.com>
2020-06-24 11:37:49 +0700

    Fix the sql so we can logged in now

--- Web-App/cek_login.php
+++ Web-App/cek_login.php
@@ -1,26 +1,25 @@
-<?php 
+<?php
 // mengaktifkan session php
 session_start();
- 
+
 // menghubungkan dengan koneksi
 include 'config.php';
- 
+
 // menangkap data yang dikirim dari form
 $E_mail = $_POST['E_mail'];
 $Pwd = $_POST['Pwd'];
- 
-// menyeleksi data admin dengan username dan password yang sesuai
-$login = mysqli_query("select * from user where E_mail='$E_mail' and Pwd='$Pwd'");
 
- 
+// menyeleksi data admin dengan username dan password yang sesuai
+$login = mysqli_query($mysqli, "SELECT * from user where E_mail='$E_mail' and Pwd='$Pwd'");
+
+
 // menghitung jumlah data yang ditemukan
 $cek = mysqli_num_rows($login);
- 
-if($cek > 0){
+
+if ($cek > 0) {
 	$_SESSION['E_mail'] = $E_mail;
 	$_SESSION['status'] = "login";
-	header("location:dashboard/index.php");
-}else{
+	header("location:dashboard/user/index.php");
+} else {
 	header("location:login.php?pesan=gagal");
 }
-?>
